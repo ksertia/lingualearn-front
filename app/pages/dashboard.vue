81081cb4e9e7d120a8dae24c92de0fce328c7397
Abdl-hafeez762 <afizouzongo115@gmail.com>
2026-01-20 11:44:10 +0000

    dashboard et graphiques OK

---
+++ app/pages/dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+    <Dashboard />
+</template>
+
+<script setup>
+import Dashboard from "~/components/Dashboard/Dashboard.vue";
+import { definePageMeta } from "#imports";
+
+definePageMeta({
+  layout: "lms",
+});
+</script>
