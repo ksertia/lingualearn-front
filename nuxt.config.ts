8cff2a730e44bccf90ec7561d41ccecfd6556a44
Donal-git <pagnagdoued@gmail.com>
2026-02-02 09:51:14 +0000

    Ajout de l'interface gestionnaire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,18 +5,30 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  css: ["~/assets/css/main.css"],
+  css: ["~/assets/css/main.css", "~/assets/css/dashboard.css"],
 
   modules: ["@pinia/nuxt"],
 
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://213.32.120.11:3200",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
   },
+
+
+
+  
   runtimeConfig: {
     public: {
-      apiBase: 'https://213.32.120.11:4000',
+      apiBase: process.env.NUXT_PUBLIC_API_URL || "http://localhost:3200",
     },
-    apiSecret: '',
   },
 
   // Redirects for deprecated routes
